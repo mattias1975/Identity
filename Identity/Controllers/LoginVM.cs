4fcaa547414d0b1d6fb04f5dc2c68b9bebf3fd38
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-08-22 16:20:54 +0200

    16:20

---
+++ Identity/Controllers/LoginVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Controllers
+{
+    public class LoginVM
+    {
+        public string Password { get; internal set; }
+        public IdentityUser UserName { get; internal set; }
+    }
+}
